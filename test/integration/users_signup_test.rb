acea43bae85663a9f6fa0baf1411d0fe4f8bc239
Cometa09 <sokolovalex09@gmail.com>
2022-04-07 13:56:47 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params:{ user: { name: "Example User",
+                                          email: "user@example.com",
+                                          password: "password",
+                                          password_confirmation: "password" }}
+       follow_redirect!                                           
+    end
+    assert_template 'users/show'
+  end
+end
+
+#!!!!!!!!!!!!!!
+# DEPRECATION WARNING: `post_via_redirect` is deprecated and will be removed in Rails 5.1.
+# Please use follow_redirect! manually after the request call for the same behavior.
+#!!!!!!!!!!!!!!
